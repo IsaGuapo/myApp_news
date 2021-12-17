d5753144e0741ae3c629e088284414716d48a1be
IsaGuapo <isabela.guapo@gmail.com>
2021-12-17 14:22:28 +0100

    add delete news from main

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,42 +1,19 @@
 
 import React, { Component } from "react";
 import Card from './Card'
-import axios from "axios";
+
 
 class List extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = { 
-      topNews:[],
-            
-       
-    }
-  }
 
-  async componentDidMount(){
-    try{
-      const newAxios = await axios.get(`https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${process.env.REACT_APP_APIKEY}`);
-      // console.log(newAxios);
-      const data = newAxios.data.results.slice(0,5)
-      const result = data.concat(this.props.stateData)
-      console.log(data)
-      this.setState({topNews: result})  
-    } catch (e){
-      console.log("error")
-    }
-       
-  
-  }
 
   paintNews = () => {
-    return this.state.topNews.map((card, i) => <Card info={card} deleteNew={() => this.deleteNew(i)} key={i} />)
+    return this.props.stateData.map((card, i) => <Card info={card} deleteNew={() => this.props.deleteNew(i)} key={i} />)
   }
 
-  deleteNew =(i) =>{
-    const card = this.state.topNews.filter((card, j) => j !== i)
-        this.setState({ topNews: card })
-  }
+  // deleteNew =(i) =>{
+  //   const card = this.state.topNews.filter((card, j) => j !== i)
+  //       this.setState({ topNews: card })
+  // }
 
   
   render() {
